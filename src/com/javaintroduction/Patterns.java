9ec1073d969dd7a9c68e37891994300ca9f69130
ramya70133 <alakuntaramya2002@gmail.com>
2025-03-09 16:03:27 +0530

    frist commit

---
+++ src/com/javaintroduction/Patterns.java
@@ -0,0 +1,20 @@
+package com.javaintroduction;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+			  pattern1(5);
+			}
+			static void pattern1(int n){
+				for(int row = 1; row <=n; row++)
+				{
+					for (int col=1; col<=row; col++ )
+					{
+						System.out.print("*");
+					}
+					   System.out.println();
+				}
+			}
+		}
+
+
---
+++ src/com/javaintroduction/Welcome.java
@@ -0,0 +1,28 @@
+package com.javaintroduction;
+
+public class Welcome {
+	  
+
+	public static void main(String[] args) {
+		System.out.println("keywords examples");
+		byte b = 127;
+		short s = 789;
+		int i = 2345678;
+		long l = 908814523;
+		float f =5678;
+		double b1= 34.897;
+		char c = 'a';
+		boolean b2 = true;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(b1);
+		System.out.println(c);
+		System.out.println(b2);
+		
+
+	}
+
+}
